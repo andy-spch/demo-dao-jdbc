1a4fbf39119a8bac0ee1afaee81892048ab4db8d
andy-spch <anderson.spch@gmail.com>
2023-08-10 14:44:30 -0300

    Reutilizing Instanciation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,10 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,6 +10,7 @@ public class Program {
 
         SellerDao sellerdao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: SELLER FIND BY ID ===");
         Seller seller = sellerdao.findById(3);
 
         System.out.println(seller);
